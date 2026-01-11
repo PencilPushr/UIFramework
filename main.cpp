e5a90c3966bf187dff45464f69f1ad3eedfeccf6
PencilPushr <ajvallee15@gmail.com>
2026-01-11 18:34:06 +0000

    Forgot what I changed

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 ﻿#include <iostream>
 
+// Step 1: Setup up WinProc and WinMessage so that windows knows how to communicate with our program.
+
+#ifndef WIN32_LEAN_AND_MEAN
+	#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <Windows.h>
+#include <stdio.h>
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+
+}
+
+// Is this necessary? Why can we not call main? we are already processing the msg's in WndProc right?
+// LRESULT WinApi WinMain()
+
 int main()
 {
-	std::cout << "Hello world!\n";
+	
 }
